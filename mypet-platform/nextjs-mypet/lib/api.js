6fbceeebb783e285eeb8b18fe4c570e60f03de56
grafeng <grfeng@theknotww.com>
2023-07-17 21:04:03 +0800

    Fix

--- mypet-platform/nextjs-mypet/lib/api.js
+++ mypet-platform/nextjs-mypet/lib/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 const ApiDriver = async (method, path, options) => {
-	const url = `http://43.138.161.53/:8080${path}`
+	const url = `http://43.138.161.53:8080${path}`
 
 	return await axios({method, url, ...options})
 }
 
 class Api {
 	async getHealth() {
-		const {data} = await ApiDriver('get', '/health?name=Admin')
+		const {data} = await ApiDriver('get', '/health?name=Test User')
 
 		return data
 	}
