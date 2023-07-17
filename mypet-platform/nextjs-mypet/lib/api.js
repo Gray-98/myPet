71c1e19df7b28854d1e7f2d7b26e8383643f7515
grafeng <grfeng@theknotww.com>
2023-07-17 17:07:34 +0800

    Fix

--- mypet-platform/nextjs-mypet/lib/api.js
+++ mypet-platform/nextjs-mypet/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ApiDriver = async (method, path, options) => {
-	const url = `http://127.0.0.1:8080${path}`
+	const url = `http://43.138.161.53/:8080${path}`
 
 	return await axios({method, url, ...options})
 }
